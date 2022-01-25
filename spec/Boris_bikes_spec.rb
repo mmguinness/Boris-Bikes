3d12daf3adea4be4330dc9becbc68ebaf6bb0d1c
Marie Mc Guinness <mcguinnessmarie@gmail.com>
2022-01-25 15:50:11 +0000

    First method add and test passed

--- spec/Boris_bikes_spec.rb
+++ spec/Boris_bikes_spec.rb
@@ -1,4 +1,10 @@
 require 'Boris_Bikes.rb'
 
 describe DockingStation do
+  describe "#release_bike" do
+    it "releases bike from station when requested" do
+      docking_station = DockingStation.new
+      expect(docking_station).to respond_to(:release_bike)
+    end
+  end
 end
