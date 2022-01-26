2017a9ee444105ffa623dbb9a6b5c52a5f627c6d
Ledders26 <benlhall584@hotmail.co.uk>
2022-01-26 15:13:46 +0000

    stage 11 first 3 tests passed

--- lib/Boris_Bikes.rb
+++ lib/Boris_Bikes.rb
@@ -1,8 +1,18 @@
 
 class DockingStation
+
   def release_bike
     Bikes.new
   end
+
+  def return_bike
+
+  end
+
+  def check_availability
+
+  end
+
 end
 
 class Bikes
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,11 +8,25 @@ describe DockingStation do
     end
   end
 
-  describe "#condition_check" do
+  describe "condition_check" do
     it "checks that the bike being release is working" do
       docking_station = DockingStation.new
       bike = docking_station.release_bike
       expect(bike.working?).to eq true 
+    end
+  end
+
+  describe "#return_bike" do
+    it "allows bike to be returned to docking station" do
+      docking_station = DockingStation.new
+      expect(docking_station).to respond_to(:return_bike)
+    end
+  end
+
+  describe "#check_availability" do
+    it "allows user to see if a bike is available" do
+      docking_station = DockingStation.new
+      expect(docking_station).to respond_to(:check_availability)
     end
   end
 end
